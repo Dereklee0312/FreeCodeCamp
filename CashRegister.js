569063d5f22d48fd994af7958f6267f101b1b93e
derek_lee <20820343@student.curtin.edu.au>
2022-11-29 18:17:04 +0400

    getting somewhere

--- CashRegister.js
+++ CashRegister.js
@@ -39,35 +39,79 @@
 function checkCashRegister(price, cash, cid) {
   let amtDue = cash - price;
   let change = [];
-  let equals = {
-    0.01: "PENNY",
-    0.05: "NICKEL",
-    0.1: "DIME",
-    0.25: "QUARTER",
-    1: "ONE",
-    5: "FIVE",
-    10: "TEN",
-    20: "TWENTY",
-    100: "ONE HUNDRED",
-  };
-  for (var i = cid.length - 1; i >= 0; i--) {
-    // Deduct amt if change available
-    if (amtDue >= cid[i][1]) {
-      change.push(cid[i]);
-      amtDue -= cid[i][1];
-    } else {
-    }
-    console.log(amtDue);
-  }
-  if (amtDue > 0) {
-    return { status: "INSUFFICIENT_FUNDS", change: [] };
-  } else if (amtDue == 0) {
-    return { status: "CLOSED", change: change };
-  }
-  return { status: "OPEN", change: change };
-}
+  let total = 0;
+  // let equals = {
+  //   0.05: "NICKEL",
+  //   0.01: "PENNY",
+  //   0.1: "DIME",
+  //   0.25: "QUARTER",
+  //   1: "ONE",
+  //   5: "FIVE",
+  //   10: "TEN",
+  //   20: "TWENTY",
+  //   100: "ONE HUNDRED",
+  // };
+  // let equals = {
+  //   NICKEL: 0.05,
+  //   PENNY: 0.01,
+  //   DIME: 0.1,
+  //   QUARTER: 0.25,
+  //   ONE: 1,
+  //   FIVE: 5,
+  //   TEN: 10,
+  //   TWENTY: 20,
+  //   "ONE HUNDRED": 100,
+  // };
 
-checkCashRegister(19.5, 20, [
+  let equals = {
+    "ONE HUNDRED": 100,
+    TWENTY: 20,
+    TEN: 10,
+    FIVE: 5,
+    ONE: 1,
+    QUARTER: 0.25,
+    DIME: 0.1,
+    NICKEL: 0.05,
+    PENNY: 0.01,
+  };
+  // for (var value in equals) {
+  //   console.log(value);
+  // }
+  //
+  // for (var i = cid.length - 1; i >= 0; i--) {
+  //   // Deduct amt if change available
+  //   if (amtDue >= cid[i][1]) {
+  //     change.push(cid[i]);
+  //     amtDue -= cid[i][1];
+  //   } else {
+  //   }
+  //   console.log(amtDue);
+  // }
+
+  for (var i = cid.length - 1; i >= 0; i--) {
+    total += cid[i][1];
+  }
+  if (total < amtDue) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  } else if (total == amtDue) {
+    return { status: "CLOSED", change: cid };
+  } else {
+    for (var i = cid.length - 1; i >= 0; i--) {
+      // console.log(cid[i][1], equals[cid[i][0]]);
+      if (amtDue > cid[i][1]) {
+        change.push(cid[i][0], cid[i][1]);
+        amtDue -= cid[i][1];
+      // } else if (amtDue < cid[i][1] && amtDue > equals[cid[i][0]]) {
+      } else if (amtDue > equals[cid[i][0]]) {
+        // console.log(amtDue, cid[i][1], equals[cid[i][0]]);
+        console.log(
+          `DUE: ${amtDue}, CID: ${cid[i][1]}, EQUALS: ${equals[cid[i][0]]}`
+        );
+      }
+    }
+  }
+}
+checkCashRegister(3.26, 100, [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
   ["DIME", 3.1],
